7d3d793e43dea722d32919f296d8b4bcc0f62181
unknown <donovant@tcd.ie>
2017-10-20 17:30:57 +0100

    Added what i have so far, tests aren't working, the project should work though

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,4 +1,82 @@
 
 public class LCA {
+	
+	Node root;
+	
+	class Node {
+
+	    String key;
+		Node left;
+		Node right;
+		
+		Node(String key) 
+		{
+			this.key = key;
+		}
+	}
+
+	
+	//take in keys as a list of values in order
+	//going root,left child, right child, left child of left child, 
+	//right child of left child, left child of right child, etc.
+	public static void createTree(String [] listOfKeys)
+	{
+		int i = 0;
+		Node newNode = null;
+		Node tempNode = null;
+		while(i <= listOfKeys.length)
+		{
+			
+			if(listOfKeys[i] != null)
+			{
+				newNode.key = listOfKeys[i];
+				tempNode = newNode;
+				if(tempNode.left == null){
+					newNode = tempNode.left;
+				}
+				else
+				{
+					newNode = tempNode.right;
+				}
+			}
+			else
+			{
+				newNode = null;
+			}
+			i++;
+		}
+	}
+	
+	public Node lowestCommonAncestor(Node rootNode, Node node1, Node node2) 
+	{
+	    if(rootNode == null)
+	    {
+	        return null;
+	    }
+	 
+	    if(rootNode == node1 || rootNode == node2)
+	    {
+	        return rootNode;
+	    }
+	 
+	    Node left = rootNode.left;
+	    Node right = rootNode.right;
+	    Node leftNode = lowestCommonAncestor(left, node1, node2);
+	    Node rightNode = lowestCommonAncestor(right, node1, node2);
+	 
+	    if(leftNode != null && rightNode != null)
+	    {
+	        return rootNode;
+	    }
+	    else if(leftNode == null && rightNode == null)
+	    {
+	        return null;
+	    }
+	    else
+	    {
+	    	//what does this do?
+	        return leftNode == null ?rightNode:leftNode;
+	    }
+	}
 
 }
--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -8,5 +8,18 @@ public class LCATest {
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Test
+    public void checkEmptyTree(){
+        String [] keys = {};
+        assertEquals(null, LCA.createTree(keys));
+    }
+
+	private void assertEquals(Object object, Object createTree) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
