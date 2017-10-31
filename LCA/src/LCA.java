542eb3f107984cc35eacd9c907c612266e07bf39
donovant95 <donovant@tcd.ie>
2017-10-31 00:29:05 +0000

    begin repo

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,84 +1,198 @@
-public class LCA {
-	
-	Node root;
-	
-	class Node {
+import java.lang.StringBuilder;
 
-	    String key;
-		Node left;
-		Node right;
+public class BST<Key extends Comparable<Key>, Value> {
+    private Node root;             // root of BST
+
+    /**
+     * Private node class.
+     */
+    private class Node {
+        private Key key;           // sorted by key
+        private Value val;         // associated data
+        private Node left, right;  // left and right subtrees
+        private int N;             // number of nodes in subtree
+
+        public Node(Key key, Value val, int N) {
+            this.key = key;
+            this.val = val;
+            this.N = N;
+        }
+    }
+    
+
+//TODO Test below function.    
+    // is the symbol table empty?
+    public boolean isEmpty() { return size() == 0; }
+
+    
+//TODO Test below function.    
+    // return number of key-value pairs in BST
+    public int size() { return size(root); }   
+    
+    // return number of key-value pairs in BST rooted at x
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.N;
+    }
+    
+    /**
+     *  Search BST for given key.
+     *  What is the value associated with given key?
+     *
+     *  @param key the search key
+     *  @return value associated with the given key if found, or null if no such key exists.
+     */
+    public Value get(Key key) { 
+    	
+    	if(key == null) return null;
+    	else return get(root, key); 
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else              return x.val;
+    }
+
+    /**
+     *  Insert key-value pair into BST.
+     *  If key already exists, update with new value.
+     *
+     *  @param key the key to insert
+     *  @param val the value associated with key
+     */
+    public void insert(Key key, Value val) {
+		if (key == null) { return; }
+        if (val == null) { delete(key); return; }
+        root = insert(root, key, val);
+    }
+
+    private Node insert(Node x, Key key, Value val) {
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left  = insert(x.left,  key, val);
+        else if (cmp > 0) x.right = insert(x.right, key, val);
+        else              x.val   = val;
+        x.N = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+    
+    /**
+     * Print all keys of the tree in a sequence, in-order.
+     * That is, for each node, the keys in the left subtree should appear before the key in the node.
+     * Also, for each node, the keys in the right subtree should appear before the key in the node.
+     * For each subtree, its keys should appear within a parenthesis.
+     *
+     * Example 1: Empty tree -- output: "()"
+     * Example 2: Tree containing only "A" -- output: "(()A())"
+     * Example 3: Tree:
+     *   B
+     *  / \
+     * A   C
+     *      \
+     *       D
+     *
+     * output: "((()A())B(()C(()D())))"
+     *
+     * output of example in the assignment: (((()A(()C()))E((()H(()M()))R()))S(()X()))
+     *
+     * @return a String with all keys in the tree, in order, parenthesised.
+     */
+    public String printTree() {
+      if (isEmpty()) return "()";
+      
+      StringBuilder output = new StringBuilder();
+      output.append("(");
+      printTree(root, output);
+      
+      String result = output.toString();
+      return result;
+      
+    }
+    
+    private StringBuilder printTree(Node x, StringBuilder output){
+    	if(x == null)
+    	{
+    		return (output.append(")"));
+    	}
+    	output.append("(");
+    	printTree(x.left, output);
+    	output.append(x.key);
+    	output.append("(");
+    	printTree(x.right, output);
+    	output.append(")");
+    	return output;
+    	
+    }
+    
+    public void delete(Key key) {
+    	if(key!=null) root = delete(root, key);
+    }
+    
+    private Node delete(Node x, Key key){
+    	if(x == null) return null;
+    	int cmp = key.compareTo(x.key);
+    	if		(cmp < 0) x.left  = delete(x.left, key);	//Search for key
+    	else if	(cmp > 0) x.right = delete(x.right, key);
+    	else
+    	{
+    		if(x.right 	== null) return x.left;				//No right child
+    		if(x.left 	== null) return x.right;			//No left child
+    		
+    		Node t = x;
+    		x = t.left;
+    		while(x.right != null)
+    		{
+    			x = x.right;
+    		}
+    		x.left = deleteMax(t.left);
+    		x.right = t.right; 		
+    	}
+    	x.N = size(x.left) + size(x.right) + 1;
+    	return x;
+    }
+    
+    /*Deletes the maximum key from a BST rooted at a given node*/    
+    private Node deleteMax(Node x)
+    {
+    	if(x.right == null) return x.left;
+    	x.right = deleteMax(x.right);
+    	x.N = 1 + size(x.left) + size(x.right);
+    	return x;
+    }
+
+    //Small function to determine if a key is present in the tree.
+    public boolean contains(Key x){
+    	return(get(x)!=null);
+    }
+    
+    
+    
+    public Key lowestCommonAncestor(Key i, Key j) {
+    	if (root == null){
+			return null;			//If tree is empty return null.
+		}
+    	if(!contains(i) || !contains(j)){
+    		return null;
+    	}
+    	return lowestCommonAncestor(root, i, j);
+    }
+    
+	private Key lowestCommonAncestor(Node x, Key i, Key j) {
 		
-		Node(String key) 
-		{
-			this.key = key;
+		int cmpI = x.key.compareTo(i);
+		int cmpJ = x.key.compareTo(j);
+		
+		if(cmpI > 0 && cmpJ > 0){
+			return lowestCommonAncestor(x.left, i, j);
 		}
-	}
-
-	
-	//take in keys as a list of values in order
-	//going root,left child, right child, left child of left child, 
-	//right child of left child, left child of right child, etc.
-	public static void createTree(String [] listOfKeys)
-	{
-		int i = 0;
-		Node newNode = null;
-		Node tempNode = null;
-		while(i <= listOfKeys.length)
-		{
-			
-			if(listOfKeys[i] != null)
-			{
-				newNode.key = listOfKeys[i];
-				tempNode = newNode;
-				if(tempNode.left == null){
-					newNode = tempNode.left;
-				}
-				else
-				{
-					newNode = tempNode.right;
-				}
-			}
-			else
-			{
-				newNode = null;
-			}
-			i++;
+		else if(cmpI < 0 && cmpJ < 0){
+			return lowestCommonAncestor(x.right, i, j);
 		}
+		else{
+			return x.key;
+		}	
 	}
-	
-	public static Node lowestCommonAncestor(Node rootNode, Node node1, Node node2) 
-	{
-	    if(rootNode == null)
-	    {
-	        return null;
-	    }
-	 
-	    if(rootNode == node1 || rootNode == node2)
-	    {
-	        return rootNode;
-	    }
-	 
-	    Node left = rootNode.left;
-	    Node right = rootNode.right;
-	    Node leftNode = lowestCommonAncestor(left, node1, node2);
-	    Node rightNode = lowestCommonAncestor(right, node1, node2);
-	 
-	    if(leftNode != null && rightNode != null)
-	    {
-	        return rootNode;
-	    }
-	    else if(leftNode == null && rightNode == null)
-	    {
-	        return null;
-	    }
-	    if(leftNode == null)
-	    {
-	        return rightNode;
-	    }
-	    else
-	    {
-	    	return leftNode;
-	    }
-	}
-
 }
--- LCA/src/LCATest.java
+++ LCA/src/LCATest.java
@@ -2,16 +2,233 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class LCATest {
+public class lowestCommonAncestorTest {
 
+	
+	
 	@Test
-	public void testCreateTree() {
-		fail("Not yet implemented");
+	public void testPrintTree()
+	{
+		//Testing printing of an empty tree.
+	     BST<Character, Character> bst = new BST<Character, Character>();
+	     assertEquals("Testing printing of empty tree",
+	             "()", bst.printTree());
+	     
+	     //Testing printing of a one node tree.
+	     bst.insert('B', 'B');
+	     assertEquals("Testing printing of one node tree",
+	             "(()B())", bst.printTree());
+	     
+	     
+	     //Testing printing of a multiple node tree/
+	     bst.insert('A', 'A');
+	     bst.insert('C', 'C');
+	     bst.insert('D', 'D');
+	     
+	     assertEquals("Testing printing of multiple node tree",
+	    		 "((()A())B(()C(()D())))", bst.printTree());	     
+	}
+	
+	@Test
+	public void testInsert() {
+		BST<Character, Character> bst = new BST<Character, Character>();
+		
+		
+		//Testing node creation.
+		bst.insert('B', 'B');
+		assertEquals("Testing insert on empty tree", 
+				"(()B())", bst.printTree());
+		
+		//Testing nodes get sorted correctly when added.
+		bst.insert('A', 'A');
+		bst.insert('C', 'C');
+		bst.insert('D', 'D');
+		
+		
+		/*
+		 * 			B
+		 * 		   / \
+		 * 		  A   C
+		 * 			   \
+		 * 				D	  
+		 */
+		
+		assertEquals("Testing insert of multiple nodes into tree", 
+				"((()A())B(()C(()D())))", bst.printTree());
+		
+		//Testing update of value in tree.
+		bst.insert('C', 'X');
+		assertEquals("Testing insert of multiple nodes into tree", 
+				'X', (char)bst.get('C'));
+		
+		//Testing insert of null value. (Should delete node)
+		bst.insert('C', null);
+		assertEquals("Testing insert of null value into tree", 
+				"((()A())B(()D()))", bst.printTree());
+		
+		//Testing insert of null key. (Should have no effect)
+		bst.insert(null, 'Q');
+		assertEquals("Testing insert of null key into tree", 
+				"((()A())B(()D()))", bst.printTree());
+		
+	}
+	
+	@Test
+	public void testGet(){
+		BST<Character, Character> bst = new BST<Character, Character>();
+		
+		assertNull("Testing get on empty tree", 
+				bst.get('A'));
+	     
+	     bst.insert('B', 'B');
+	     bst.insert('C', 'C');
+	     bst.insert('D', 'D');
+	     bst.insert('E', 'E');
+	     
+	     assertNull("Testing get on multiple node tree that doesn't contain key",
+	             bst.get('A'));   
+	     
+
+	     assertEquals("Testing get on multiple node tree that does contain key",
+	             'C', (char)bst.get('C'));   //Have to cast to char as will return type of 'Value'.
+	     
+	     assertNull("Testing null get", bst.get(null));
+	      
+	}	
+	
+	@Test
+	public void testDelete(){
+		BST<Integer, Integer> bst = new BST<Integer, Integer>();
+		
+		//Testing delete on an empty tree.
+		bst.delete(1);
+        assertEquals("Deleting from empty tree", "()", bst.printTree());
+        
+        
+        bst.insert(7, 7);   //        _7_
+        bst.insert(8, 8);   //      /     \
+        bst.insert(3, 3);   //    _3_      8
+        bst.insert(1, 1);   //  /     \
+        bst.insert(2, 2);   // 1       6
+        bst.insert(6, 6);   //  \     /
+        bst.insert(4, 4);   //   2   4
+        bst.insert(5, 5);   //        \
+        					//         5
+        
+        assertEquals("Testing constructed tree is as expected.",
+                "(((()1(()2()))3((()4(()5()))6()))7(()8()))", bst.printTree());
+        
+        //Test deleting key not present in tree. Should have no effect.
+        bst.delete(9);
+        assertEquals("Deleting non-existent key",
+                "(((()1(()2()))3((()4(()5()))6()))7(()8()))", bst.printTree());
+
+        //Test deleting null key. Should have no effect.
+        bst.delete(null);
+        assertEquals("Deleting non-existent key",
+                "(((()1(()2()))3((()4(()5()))6()))7(()8()))", bst.printTree());
+        
+        //Test deleting leaf.
+        bst.delete(8);
+        assertEquals("Deleting leaf", "(((()1(()2()))3((()4(()5()))6()))7())", bst.printTree());
+
+        //Test deleting node with single child.
+        bst.delete(6);
+        assertEquals("Deleting node with single child",
+                "(((()1(()2()))3(()4(()5())))7())", bst.printTree());
+
+        //Test deleting node with two children
+        bst.delete(3);
+        assertEquals("Deleting node with two children",
+                "(((()1())2(()4(()5())))7())", bst.printTree());
+	}
+
+	
+	@Test
+	public void testLowestCommonAncestor(){
+		//Lowest Common Ancestor should require two keys and return key of LCA. We then can use 
+		
+		
+		BST<Integer, Integer> bst = new BST<Integer, Integer>();
+		
+		//Testing empty tree.
+		assertNull("Testing LCA on empty tree", bst.lowestCommonAncestor(1, 2));
+		
+		//Testing one-node tree.
+		bst.insert(1,1);
+		
+		//If given non-present keys - should return null.
+		
+		//One key present.
+		assertNull("Testing one node tree given non-present keys", bst.lowestCommonAncestor(2,1));
+		
+		//Both non-present keys.
+		assertNull("Testing one node tree given non-present keys", bst.lowestCommonAncestor(2,3));
+		
+		//Testing multi-node tree
+		BST<Integer, Integer> bst2 = new BST<Integer, Integer>();
+
+	       	bst2.insert(7, 7);   //        _7_
+	        bst2.insert(8, 8);   //      /     \
+	        bst2.insert(3, 3);   //    _3_      8
+	        bst2.insert(1, 1);   //  /     \
+	        bst2.insert(2, 2);   // 1       6
+	        bst2.insert(6, 6);   //  \     /
+	        bst2.insert(4, 4);   //   2   4
+	        bst2.insert(5, 5);   //        \
+	        					 //         5
+		
+	    //If either given key is the root, should return the root
+		assertEquals("Testing one node tree given root key", 7, (int)bst2.lowestCommonAncestor(7,2));    
+	        
+	    assertEquals("Testing multi-node tree", 7, (int)bst2.lowestCommonAncestor(3,8));
+	    assertEquals("Testing multi-node tree", 7, (int)bst2.lowestCommonAncestor(5,8));
+	    assertEquals("Testing multi-node tree", 3, (int)bst2.lowestCommonAncestor(3,6));
+	    assertEquals("Testing multi-node tree", 3, (int)bst2.lowestCommonAncestor(2,5));
+	}
+	
+	@Test
+	public void testIsEmpty(){
+		BST<Integer, Integer> bst = new BST<Integer, Integer>();
+		assertEquals("Testing empty tree", true, bst.isEmpty());
+		
+		bst.insert(1, 1);
+		assertEquals("Testing non-empty tree", false, bst.isEmpty());	
+	}
+	
+	@Test
+	public void testSize(){
+		BST<Integer, Integer> bst = new BST<Integer, Integer>();
+		assertEquals("Testing empty tree", 0, bst.size());
+		
+		bst.insert(1, 1);
+		assertEquals("Testing single node tree", 1, bst.size());
+		
+		bst.insert(2, 2);
+		bst.insert(7, 7);
+		bst.insert(3, 3);
+		bst.insert(99, 99);
+		
+		assertEquals("Testing multi node tree", 5, bst.size());
 	}
 
 	@Test
-	public void testLowestCommonAncestor() {
-		fail("Not yet implemented");
+	public void testContains(){
+		BST<Integer, Integer> bst = new BST<Integer, Integer>();
+		assertEquals("Testing empty tree", false, bst.contains(4));
+		
+		bst.insert(1, 1);
+		assertEquals("Testing single node tree containing key", true, bst.contains(1));
+		assertEquals("Testing single node tree not containing key", false, bst.contains(5));
+		
+		bst.insert(2, 2);
+		bst.insert(7, 7);
+		bst.insert(3, 3);
+		bst.insert(99, 99);
+		
+		assertEquals("Testing multi node tree containing key", true, bst.contains(99));
+		assertEquals("Testing multi node tree containing key", false, bst.contains(4));
+		
+		assertEquals("Testing contains null. Should return false", false,  bst.contains(null));
 	}
-
 }
